1044bb2e1e52d1ddd248584f865920116371a9e7
Daniel Power <me@danielpower.ca>
2023-05-04 18:24:06 -0230

    Changes

--- terraform.tf
+++ terraform.tf
@@ -8,39 +8,46 @@ terraform {
 }
 
 variable "do_token" {}
+variable "ssh_key" {}
 
 provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_droplet" "web" {
+resource "digitalocean_droplet" "main" {
   image     = "fedora-37-x64"
   name      = "personal-site"
   region    = "tor1"
   size      = "s-1vcpu-1gb"
   user_data = file("${path.module}/cloud-config.yaml")
+  ssh_keys  = [digitalocean_ssh_key.main.fingerprint]
 }
 
-resource "digitalocean_domain" "domain" {
+resource "digitalocean_ssh_key" "main" {
+  name       = "personal-site"
+  public_key = var.ssh_key
+}
+
+resource "digitalocean_domain" "main" {
   name = "danielpower.ca"
 }
 
 resource "digitalocean_record" "main" {
-  domain = digitalocean_domain.domain.name
+  domain = digitalocean_domain.main.name
   type   = "A"
   name   = "@"
-  value  = digitalocean_droplet.web.ipv4_address
+  value  = digitalocean_droplet.main.ipv4_address
 }
 
 resource "digitalocean_record" "email_cname" {
-  domain = digitalocean_domain.domain.name
+  domain = digitalocean_domain.main.name
   type   = "CNAME"
   name   = "mail"
   value  = "mail.hover.com.cust.hostedemail.com."
 }
 
 resource "digitalocean_record" "email_mx" {
-  domain   = digitalocean_domain.domain.name
+  domain   = digitalocean_domain.main.name
   type     = "MX"
   name     = "@"
   value    = "mx.hover.com.cust.hostedemail.com."
