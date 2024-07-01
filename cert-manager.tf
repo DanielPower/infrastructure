d6db655a5e9707b661856d203fba1d64507f89fb
Daniel Power <me@danielpower.ca>
2024-07-01 20:39:57 -0230

    Introducing the homelab cluster

---
+++ cert-manager.tf
@@ -0,0 +1,77 @@
+resource "kubernetes_namespace" "cert_manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "helm_release" "cert_manager" {
+  name      = "cert-manager"
+  namespace = kubernetes_namespace.cert_manager.id
+
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "v1.15.1"
+
+  set {
+    name  = "installCRDs"
+    value = true
+  }
+}
+
+resource "kubernetes_manifest" "letsencrypt_staging_issuer" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+    metadata = {
+      name = "letsencrypt-staging"
+    }
+
+    spec = {
+      acme = {
+        email  = "me@danielpower.ca"
+        server = "https://acme-staging-v02.api.letsencrypt.org/directory"
+        privateKeySecretRef = {
+          name = "letsencrypt-staging-account-key"
+        }
+        solvers = [
+          {
+            http01 = {
+              ingress = {
+                ingressClassName = "nginx"
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "letsencrypt_issuer" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+    metadata = {
+      name = "letsencrypt"
+    }
+
+    spec = {
+      acme = {
+        email  = "me@danielpower.ca"
+        server = "https://acme-v02.api.letsencrypt.org/directory"
+        privateKeySecretRef = {
+          name = "letsencrypt-account-key"
+        }
+        solvers = [
+          {
+            http01 = {
+              ingress = {
+                ingressClassName = "nginx"
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+}
---
+++ csi.tf
@@ -0,0 +1,22 @@
+resource "helm_release" "csi_nfs" {
+  name      = "csi-driver-nfs"
+  namespace = "kube-system"
+
+  repository = "https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts"
+  chart      = "csi-driver-nfs"
+  version    = "v4.7.0"
+}
+
+resource "kubernetes_storage_class" "nfs" {
+  metadata {
+    name = "nfs-csi"
+  }
+
+  storage_provisioner = "nfs.csi.k8s.io"
+
+  parameters = {
+    server = "192.168.0.2"
+    share  = "/main/crispykube"
+    subDir = "$${pvc.metadata.namespace}/$${pvc.metadata.name}"
+  }
+}
