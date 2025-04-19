cd00f66f32739595022086f20b109a548598cbd7
Daniel Power <me@danielpower.ca>
2025-04-19 16:33:38 -0230

    Cleanup

--- terraform/talos.tf
+++ terraform/talos.tf
@@ -6,7 +6,6 @@ data "talos_client_configuration" "this" {
 }
 
 data "talos_machine_configuration" "control_plane" {
-  for_each         = var.control_plane_nodes
   cluster_name     = var.cluster_name
   machine_secrets  = talos_machine_secrets.this.machine_secrets
   cluster_endpoint = var.cluster_endpoint
@@ -14,7 +13,6 @@ data "talos_machine_configuration" "control_plane" {
 }
 
 data "talos_machine_configuration" "worker" {
-  for_each         = var.worker_nodes
   cluster_name     = var.cluster_name
   machine_secrets  = talos_machine_secrets.this.machine_secrets
   cluster_endpoint = var.cluster_endpoint
@@ -24,7 +22,7 @@ data "talos_machine_configuration" "worker" {
 resource "talos_machine_configuration_apply" "control_plane" {
   for_each                    = var.control_plane_nodes
   client_configuration        = data.talos_client_configuration.this.client_configuration
-  machine_configuration_input = data.talos_machine_configuration.control_plane[each.key].machine_configuration
+  machine_configuration_input = data.talos_machine_configuration.control_plane.machine_configuration
   node                        = each.value.endpoint
   config_patches              = [
     templatefile("./talos/templates/hostname.yaml", { hostname = each.value.hostname })
@@ -34,7 +32,7 @@ resource "talos_machine_configuration_apply" "control_plane" {
 resource "talos_machine_configuration_apply" "workers" {
   for_each                    = var.worker_nodes
   client_configuration        = data.talos_client_configuration.this.client_configuration
-  machine_configuration_input = data.talos_machine_configuration.worker[each.key].machine_configuration
+  machine_configuration_input = data.talos_machine_configuration.worker.machine_configuration
   node                        = each.value.endpoint
   config_patches              = [
     templatefile("./talos/templates/hostname.yaml", { hostname = each.value.hostname })
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "talos_config_path" {
-  description = "Path to the Talos configuration file"
-  type        = string
-  default     = "../talos/talosconfig"
-}
-
 variable "cluster_name" {
   description = "Name of the Talos cluster"
   type        = string
@@ -16,43 +10,12 @@ variable "cluster_endpoint" {
   default     = "https://192.168.0.10:6443"
 }
 
-variable "talos_version" {
-  description = "Talos version"
-  type        = string
-  default     = "v1.9.4"
-}
-
-variable "talos_machine_secrets" {
-  description = "Talos machine secrets"
-  type        = object({
-    id = string
-    machine_secrets = object({
-      certs = string
-      cluster = string
-      secrets = string
-      trustdinfo = string
-    })
-    client_configuration = object({
-      ca_certificate = string
-      client_certificate = string
-      client_key = string
-    })
-  })
-}
-
-variable "kubernetes_version" {
-  description = "Kubernetes version"
-  type        = string
-  default     = "v1.30.1"
-}
-
 variable "control_plane_nodes" {
   description = "Map of control plane nodes"
   type = map(object({
     hostname = string
     endpoint = string
     ip       = string
-    disk     = optional(string)
   }))
 }
 
@@ -62,6 +25,5 @@ variable "worker_nodes" {
     hostname = string
     endpoint = string
     ip       = string
-    disk     = optional(string)
   }))
 }
