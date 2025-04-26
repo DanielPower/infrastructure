4b786ab36bb8d6369c1c889da73fcfbd6c0b2638
Daniel Power <me@danielpower.ca>
2025-04-25 21:52:33 -0230

    Add nodes

--- terraform/talos.tf
+++ terraform/talos.tf
@@ -1,20 +1,23 @@
 locals {
-  cluster_name = "homelab"
+  cluster_name     = "homelab"
   cluster_endpoint = "https://192.168.0.11:6443"
 
   control_plane_nodes = {
     "linie" = {
       hostname = "controlplane-linie"
+      disk     = "/dev/nvme0n1"
       endpoint = "192.168.0.11"
       ip       = "192.168.0.11"
     },
     "draht" = {
       hostname = "controlplane-draht"
+      disk     = "/dev/nvme0n1"
       endpoint = "192.168.0.13"
       ip       = "192.168.0.13"
     },
     "lugner" = {
       hostname = "controlplane-lugner"
+      disk     = "/dev/nvme0n1"
       endpoint = "192.168.0.14"
       ip       = "192.168.0.14"
     }
@@ -22,8 +25,39 @@ locals {
   worker_nodes = {
     "heiter" = {
       hostname = "worker-heiter"
+      disk     = "/dev/nvme0n1"
       endpoint = "192.168.0.10"
       ip       = "192.168.0.10"
+    }
+    "lawine" = {
+      hostname = "worker-lawine"
+      disk     = "/dev/nvme0n1"
+      endpoint = "192.168.0.15"
+      ip       = "192.168.0.15"
+    }
+    "kanne" = {
+      hostname = "worker-kanne"
+      disk     = "/dev/nvme0n1"
+      endpoint = "192.168.0.16"
+      ip       = "192.168.0.16"
+    }
+    "scharf" = {
+      hostname = "worker-scharf"
+      disk     = "/dev/nvme0n1"
+      endpoint = "192.168.0.17"
+      ip       = "192.168.0.17"
+    }
+    "ehre" = {
+      hostname = "worker-ehre"
+      disk     = "/dev/nvme0n1"
+      endpoint = "192.168.0.18"
+      ip       = "192.168.0.18"
+    }
+    "richter" = {
+      hostname = "worker-richter"
+      disk     = "/dev/nvme0n1"
+      endpoint = "192.168.0.19"
+      ip       = "192.168.0.19"
     }
   }
 }
@@ -31,7 +65,7 @@ locals {
 resource "talos_machine_secrets" "this" {}
 
 data "talos_client_configuration" "this" {
-  cluster_name        = local.cluster_name
+  cluster_name         = local.cluster_name
   client_configuration = talos_machine_secrets.this.client_configuration
 }
 
@@ -54,8 +88,12 @@ resource "talos_machine_configuration_apply" "control_plane" {
   client_configuration        = data.talos_client_configuration.this.client_configuration
   machine_configuration_input = data.talos_machine_configuration.control_plane.machine_configuration
   node                        = each.value.endpoint
-  config_patches              = [
-    templatefile("./talos/templates/hostname.yaml", { hostname = each.value.hostname })
+  config_patches = [
+    templatefile("./talos/templates/hostname.yaml", { hostname = each.value.hostname }),
+    templatefile("./talos/templates/disk.yaml", {
+      disk  = each.value.disk,
+      image = ""
+    })
   ]
 }
 
@@ -64,7 +102,11 @@ resource "talos_machine_configuration_apply" "workers" {
   client_configuration        = data.talos_client_configuration.this.client_configuration
   machine_configuration_input = data.talos_machine_configuration.worker.machine_configuration
   node                        = each.value.endpoint
-  config_patches              = [
-    templatefile("./talos/templates/hostname.yaml", { hostname = each.value.hostname })
+  config_patches = [
+    templatefile("./talos/templates/hostname.yaml", { hostname = each.value.hostname }),
+    templatefile("./talos/templates/disk.yaml", {
+      disk  = each.value.disk,
+      image = lookup(each.value, "image", "")
+    })
   ]
 }
