033bb42c38b1c163a8606a415b6b8608db712161
Daniel Power <me@danielpower.ca>
2025-06-07 13:16:43 -0230

    Update talos.tf

--- terraform/talos.tf
+++ terraform/talos.tf
@@ -1,58 +1,50 @@
 locals {
   cluster_name     = "homelab"
   cluster_endpoint = "https://192.168.0.11:6443"
-  talos_version = "1.9.5"
+  talos_version    = "1.9.5"
 
   control_plane_nodes = {
     "linie" = {
-      hostname = "controlplane-linie"
       disk     = "/dev/nvme0n1"
       endpoint = "192.168.0.11"
       ip       = "192.168.0.11"
     },
     "draht" = {
-      hostname = "controlplane-draht"
       disk     = "/dev/nvme0n1"
       endpoint = "192.168.0.13"
       ip       = "192.168.0.13"
     },
     "lugner" = {
-      hostname = "controlplane-lugner"
       disk     = "/dev/nvme0n1"
       endpoint = "192.168.0.14"
       ip       = "192.168.0.14"
     }
   }
   worker_nodes = {
-    "heiter" = {
-      hostname = "worker-heiter"
-      disk     = "/dev/nvme0n1"
-      endpoint = "192.168.0.10"
-      ip       = "192.168.0.10"
-    }
-    "lawine" = {
-      hostname = "worker-lawine"
+    "attlerock" = {
       disk     = "/dev/nvme0n1"
       endpoint = "192.168.0.15"
       ip       = "192.168.0.15"
     }
-    "kanne" = {
-      hostname = "worker-kanne"
+    "timber-hearth" = {
       disk     = "/dev/nvme0n1"
       endpoint = "192.168.0.16"
       ip       = "192.168.0.16"
     }
-    "scharf" = {
-      hostname = "worker-scharf"
+    "giants-deep" = {
       disk     = "/dev/nvme0n1"
       endpoint = "192.168.0.17"
       ip       = "192.168.0.17"
     }
-    "ehre" = {
-      hostname = "worker-ehre"
+    "dark-bramble" = {
       disk     = "/dev/nvme0n1"
       endpoint = "192.168.0.18"
       ip       = "192.168.0.18"
+    }
+    "brittle-hollow" = {
+      disk     = "/dev/nvme0n1"
+      endpoint = "192.168.0.19"
+      ip       = "192.168.0.19"
     }
   }
 }
@@ -76,7 +68,7 @@ data "talos_machine_configuration" "worker" {
   cluster_name     = local.cluster_name
   machine_secrets  = talos_machine_secrets.this.machine_secrets
   cluster_endpoint = local.cluster_endpoint
-  talos_version = local.talos_version
+  talos_version    = local.talos_version
   machine_type     = "worker"
 }
 
@@ -86,9 +78,9 @@ resource "talos_machine_configuration_apply" "control_plane" {
   machine_configuration_input = data.talos_machine_configuration.control_plane.machine_configuration
   node                        = each.value.endpoint
   config_patches = [
-    templatefile("./talos/templates/hostname.yaml", { hostname = each.value.hostname }),
+    templatefile("./talos/templates/hostname.yaml", { hostname = each.key }),
     templatefile("./talos/templates/disk.yaml", {
-      disk  = each.value.disk,
+      disk = each.value.disk,
     })
   ]
 }
@@ -99,9 +91,9 @@ resource "talos_machine_configuration_apply" "workers" {
   machine_configuration_input = data.talos_machine_configuration.worker.machine_configuration
   node                        = each.value.endpoint
   config_patches = [
-    templatefile("./talos/templates/hostname.yaml", { hostname = each.value.hostname }),
+    templatefile("./talos/templates/hostname.yaml", { hostname = each.key }),
     templatefile("./talos/templates/disk.yaml", {
-      disk  = each.value.disk,
+      disk = each.value.disk,
     })
   ]
 }
